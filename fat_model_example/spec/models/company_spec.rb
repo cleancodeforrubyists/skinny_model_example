f3362193df62187f6d8426aee8854c80fd442579
Carleton DiLeo <cbdileo@gmail.com>
2014-11-16 17:02:03 -0800

    Small modifications to the specs to improve structure

--- fat_model_example/spec/models/company_spec.rb
+++ fat_model_example/spec/models/company_spec.rb
@@ -1,31 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Company do
-  context 'less than 500 employees' do
-    before do
-      subject.save!
+  context '#small_business?' do
+    before { subject.save! }
 
-      10.times do
-        subject.employees.create!
+    context 'less than 500 employees' do
+      before do
+        10.times { subject.employees.create! }
+      end
+
+      specify 'is a small business' do
+        expect(subject).to be_small_business
       end
     end
 
-    specify 'is a small business' do
-      expect(subject).to be_small_business
-    end
-  end
-
-  context 'more than 500 employees' do
-    before do
-      subject.save!
-
-      501.times do
-        subject.employees.create!
+    context 'more than 500 employees' do
+      before do
+        501.times { subject.employees.create! }
       end
-    end
 
-    specify 'is a small business' do
-      expect(subject).to_not be_small_business
+      specify 'is not a small business' do
+        expect(subject).to_not be_small_business
+      end
     end
   end
 end
--- skinny_model_example/spec/models/company_size_standard_spec.rb
+++ skinny_model_example/spec/models/company_size_standard_spec.rb
@@ -1,24 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe CompanySizeStandard do
-  let(:company) { double(:company, employees: employees) }
-
   subject { CompanySizeStandard.new(company) }
 
-  context 'less than 500 employees' do
-    let(:employees) { double(:employees, count: 10) }
+  context '#small?' do
+    let(:company) { double(:company, employees: employees) }
 
-    specify 'is a small business' do
-      expect(subject).to be_small
+    context 'less than 500 employees' do
+      let(:employees) { double(:employees, count: 10) }
+
+      specify 'is a small business' do
+        expect(subject).to be_small
+      end
     end
-  end
 
+    context 'more than 500 employees' do
+      let(:employees) { double(:employees, count: 501) }
 
-  context 'more than 500 employees' do
-    let(:employees) { double(:employees, count: 501) }
-
-    specify 'is a small business' do
-      expect(subject).to_not be_small
+      specify 'is a small business' do
+        expect(subject).to_not be_small
+      end
     end
   end
 end
